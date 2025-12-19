376637ccef1326ee2a1a1a3e400db9321b232921
Lena Bykanowa <lena.bykanowa@gmail.com>
2025-12-19 13:14:54 +0100

    AKO4: plus_jeden

--- cwiczenie1.asm
+++ cwiczenie1.asm
@@ -1,41 +1,22 @@
 .686
 .model flat
 
-public _szukaj_max
+public _plus_jeden
 
 .code
+_plus_jeden PROC
+	push	ebp			; zapisanie wartoœci EBP na stosie
+	mov		ebp, esp	; kopiowanie zawartoœci ESP do EBP
+	push	ebx			; przechowanie zawartoœci ebx
+; wpisanie do rejestru EBX adresu zmiennej zdefiniowanej
+; w kodzie w jêzyku C
+	mov		ebx, [ebp+8]
+	mov		eax, [ebx]  ; odczytanie wartoœci zmiennej
+	inc		eax
+	mov		[ebx], eax
 
-_szukaj_max PROC
-	push	ebp			; zapisanie zawartoœci EBP na stosie
-	mov		ebp, esp	; kopiowanie zawartosci ESP do EBP
-
-	mov		eax, [ebp+8]	; liczba x
-	cmp		eax, [ebp+12]	; porownanie liczb x i y
-	jge		x_wieksza		; skok, gdy x >= y
-
-; przypadek, gdy x < y
-	mov		eax, [ebp+12]	; liczba y
-	cmp		eax, [ebp+16]	; porownanie liczb y i z
-	jge		y_wieksza		; skok, gdy y>=z
-
-; przypadek y < z
-; z jest najwiêksza
-wypisz_z: 
-	mov		eax, [ebp+16]	; liczba z
-
-zakoncz:
+	pop		ebx
 	pop		ebp
 	ret
-
-x_wieksza:
-	cmp		eax, [ebp+16]	; porownanie x i z
-	jge		zakoncz			; skok, gdy x>=z
-	jmp		wypisz_z		
-
-
-y_wieksza:
-	mov		eax, [ebp+12]	; liczba y
-	jmp		zakoncz
-
-_szukaj_max	ENDP
-			END
+_plus_jeden	ENDP
+END
--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 # include <stdio.h>
 # include <locale.h>
-int szukaj4_max(int a, int b, int c, int d);
+void plus_jeden(int* a);
 
 int main() {
-	setlocale(LC_CTYPE, "Polish");
-	int a, b, c, d, wynik;
-	printf("\nProszê podaæ trzy liczby ca³kowite ze znakiem: ");
-	scanf_s("%d %d %d %d", &a, &b, &c, &d, 32);
+	//setlocale(LC_CTYPE, "Polish");
+	int m;
+	m = -5;
+	plus_jeden(&m);
 
-	wynik = szukaj4_max(a, b, c, d);
-
-	printf("\Spoœród podanych liczb %d, %d, %d, %d \
-	liczba %d jest najwiêksza\n", a, b, c, d, wynik);
-
+	printf("\n m = %d\n", m);
 	return 0;
 }
